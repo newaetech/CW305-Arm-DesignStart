450dab0cca03b691732f5289cf1e296c18a18fee
Gijs Burghoorn <g.burghoorn@gmail.com>
2022-07-07 10:34:12 +0200

    Revert unnecesarry whitespace changes

--- src/hardware/CW305_designstart_top.v
+++ src/hardware/CW305_designstart_top.v
@@ -1,10 +1,12 @@
 /* 
 Copyright (c) 2019-2020, NewAE Technology Inc.
 All rights reserved.
+
 Redistribution and use in source and binary forms, with or without
 modification, are permitted without restriction. Note that modules within
 the project may have additional restrictions, please carefully inspect
 additional licenses.
+
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
 ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -15,6 +17,7 @@ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
 ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 The views and conclusions contained in the software and documentation are those
 of the authors and should not be interpreted as representing official policies,
 either expressed or implied, of NewAE Technology Inc.
@@ -253,4 +256,3 @@ module CW305_designstart_top #(
 
 endmodule
 `default_nettype wire
-
